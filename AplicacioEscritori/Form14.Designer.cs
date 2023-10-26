70d85c78aca6e859338771c7c175c9241d872c18
ASBcep <asaboritb2324@politecnics.barcelona>
2023-10-26 18:42:49 +0200

    Formulari 14 dissenyat

--- AplicacioEscritori/Form14.Designer.cs
+++ AplicacioEscritori/Form14.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace AplicacioEscritori
+﻿namespace Formulari_14
 {
-    partial class Form14
+    partial class Formulari_14
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,153 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1Resultat = new System.Windows.Forms.GroupBox();
+            this.radioButton1ZeroQuatre = new System.Windows.Forms.RadioButton();
+            this.radioButton2CincVuit = new System.Windows.Forms.RadioButton();
+            this.radioButton3NouDeu = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2ImatgeVehicle = new System.Windows.Forms.GroupBox();
+            this.button2Enrere = new System.Windows.Forms.Button();
+            this.button3ConfirmarCanvis = new System.Windows.Forms.Button();
+            this.button1SeleccionarImatge = new System.Windows.Forms.Button();
+            this.groupBox1Resultat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1Resultat
+            // 
+            this.groupBox1Resultat.Controls.Add(this.radioButton3NouDeu);
+            this.groupBox1Resultat.Controls.Add(this.radioButton2CincVuit);
+            this.groupBox1Resultat.Controls.Add(this.radioButton1ZeroQuatre);
+            this.groupBox1Resultat.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1Resultat.Name = "groupBox1Resultat";
+            this.groupBox1Resultat.Size = new System.Drawing.Size(234, 46);
+            this.groupBox1Resultat.TabIndex = 0;
+            this.groupBox1Resultat.TabStop = false;
+            this.groupBox1Resultat.Text = "Resultat:";
+            // 
+            // radioButton1ZeroQuatre
+            // 
+            this.radioButton1ZeroQuatre.AutoSize = true;
+            this.radioButton1ZeroQuatre.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1ZeroQuatre.Name = "radioButton1ZeroQuatre";
+            this.radioButton1ZeroQuatre.Size = new System.Drawing.Size(40, 17);
+            this.radioButton1ZeroQuatre.TabIndex = 0;
+            this.radioButton1ZeroQuatre.TabStop = true;
+            this.radioButton1ZeroQuatre.Text = "0-4";
+            this.radioButton1ZeroQuatre.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2CincVuit
+            // 
+            this.radioButton2CincVuit.AutoSize = true;
+            this.radioButton2CincVuit.Location = new System.Drawing.Point(52, 19);
+            this.radioButton2CincVuit.Name = "radioButton2CincVuit";
+            this.radioButton2CincVuit.Size = new System.Drawing.Size(40, 17);
+            this.radioButton2CincVuit.TabIndex = 1;
+            this.radioButton2CincVuit.TabStop = true;
+            this.radioButton2CincVuit.Text = "5-8";
+            this.radioButton2CincVuit.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3NouDeu
+            // 
+            this.radioButton3NouDeu.AutoSize = true;
+            this.radioButton3NouDeu.Location = new System.Drawing.Point(98, 19);
+            this.radioButton3NouDeu.Name = "radioButton3NouDeu";
+            this.radioButton3NouDeu.Size = new System.Drawing.Size(46, 17);
+            this.radioButton3NouDeu.TabIndex = 2;
+            this.radioButton3NouDeu.TabStop = true;
+            this.radioButton3NouDeu.Text = "9-10";
+            this.radioButton3NouDeu.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escollir vehicle assignat:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox2ImatgeVehicle
+            // 
+            this.groupBox2ImatgeVehicle.Location = new System.Drawing.Point(12, 95);
+            this.groupBox2ImatgeVehicle.Name = "groupBox2ImatgeVehicle";
+            this.groupBox2ImatgeVehicle.Size = new System.Drawing.Size(235, 100);
+            this.groupBox2ImatgeVehicle.TabIndex = 2;
+            this.groupBox2ImatgeVehicle.TabStop = false;
+            this.groupBox2ImatgeVehicle.Text = "Imatge Seleccionada:";
+            // 
+            // button2Enrere
+            // 
+            this.button2Enrere.Location = new System.Drawing.Point(13, 201);
+            this.button2Enrere.Name = "button2Enrere";
+            this.button2Enrere.Size = new System.Drawing.Size(75, 23);
+            this.button2Enrere.TabIndex = 3;
+            this.button2Enrere.Text = "Enrere";
+            this.button2Enrere.UseVisualStyleBackColor = true;
+            // 
+            // button3ConfirmarCanvis
+            // 
+            this.button3ConfirmarCanvis.Location = new System.Drawing.Point(172, 201);
+            this.button3ConfirmarCanvis.Name = "button3ConfirmarCanvis";
+            this.button3ConfirmarCanvis.Size = new System.Drawing.Size(75, 23);
+            this.button3ConfirmarCanvis.TabIndex = 4;
+            this.button3ConfirmarCanvis.Text = "Confirmar canvis";
+            this.button3ConfirmarCanvis.UseVisualStyleBackColor = true;
+            // 
+            // button1SeleccionarImatge
+            // 
+            this.button1SeleccionarImatge.Location = new System.Drawing.Point(163, 66);
+            this.button1SeleccionarImatge.Name = "button1SeleccionarImatge";
+            this.button1SeleccionarImatge.Size = new System.Drawing.Size(84, 23);
+            this.button1SeleccionarImatge.TabIndex = 5;
+            this.button1SeleccionarImatge.Text = "Seleccionar...";
+            this.button1SeleccionarImatge.UseVisualStyleBackColor = true;
+            // 
+            // Formulari_14
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form14";
+            this.ClientSize = new System.Drawing.Size(259, 236);
+            this.Controls.Add(this.button1SeleccionarImatge);
+            this.Controls.Add(this.button3ConfirmarCanvis);
+            this.Controls.Add(this.button2Enrere);
+            this.Controls.Add(this.groupBox2ImatgeVehicle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1Resultat);
+            this.Name = "Formulari_14";
+            this.Text = "Formulari 14";
+            this.groupBox1Resultat.ResumeLayout(false);
+            this.groupBox1Resultat.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1Resultat;
+        private System.Windows.Forms.RadioButton radioButton3NouDeu;
+        private System.Windows.Forms.RadioButton radioButton2CincVuit;
+        private System.Windows.Forms.RadioButton radioButton1ZeroQuatre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2ImatgeVehicle;
+        private System.Windows.Forms.Button button2Enrere;
+        private System.Windows.Forms.Button button3ConfirmarCanvis;
+        private System.Windows.Forms.Button button1SeleccionarImatge;
     }
 }
+
