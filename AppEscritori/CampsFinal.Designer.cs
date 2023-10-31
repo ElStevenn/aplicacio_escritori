11c9d2dd2724098c74d3d7a43ae102a0a3ba728d
LeoFerraz1903 <lferrazm2122@politecnics.barcelona>
2023-10-31 18:57:47 +0100

    Form 5 CampsFinal

---
+++ AppEscritori/CampsFinal.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace AppEscritori
+{
+    partial class CampsFinal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox_from5 = new TextBox();
+            textBox_selecciona = new TextBox();
+            comboBox_selecciona = new ComboBox();
+            textBox_selecciona2 = new TextBox();
+            groupBox_RadioButtons = new GroupBox();
+            radioButtoningles = new RadioButton();
+            radioButtoncatala = new RadioButton();
+            radioButton_castellano = new RadioButton();
+            textBox_selecciona3 = new TextBox();
+            comboBox_selecciona_tablet = new ComboBox();
+            checkBox_element_inicial = new CheckBox();
+            button_atras = new Button();
+            button_generar = new Button();
+            groupBox_RadioButtons.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox_from5
+            // 
+            textBox_from5.Location = new Point(8, 8);
+            textBox_from5.Margin = new Padding(2, 2, 2, 2);
+            textBox_from5.Name = "textBox_from5";
+            textBox_from5.Size = new Size(84, 23);
+            textBox_from5.TabIndex = 0;
+            textBox_from5.Text = "Formulario 5";
+            // 
+            // textBox_selecciona
+            // 
+            textBox_selecciona.Location = new Point(7, 34);
+            textBox_selecciona.Margin = new Padding(2, 2, 2, 2);
+            textBox_selecciona.Name = "textBox_selecciona";
+            textBox_selecciona.Size = new Size(246, 23);
+            textBox_selecciona.TabIndex = 1;
+            textBox_selecciona.Text = "Selecciona el elemento más proximo";
+            // 
+            // comboBox_selecciona
+            // 
+            comboBox_selecciona.FormattingEnabled = true;
+            comboBox_selecciona.Location = new Point(7, 64);
+            comboBox_selecciona.Margin = new Padding(2, 2, 2, 2);
+            comboBox_selecciona.Name = "comboBox_selecciona";
+            comboBox_selecciona.Size = new Size(246, 23);
+            comboBox_selecciona.TabIndex = 2;
+            // 
+            // textBox_selecciona2
+            // 
+            textBox_selecciona2.Location = new Point(7, 95);
+            textBox_selecciona2.Margin = new Padding(2, 2, 2, 2);
+            textBox_selecciona2.Name = "textBox_selecciona2";
+            textBox_selecciona2.Size = new Size(246, 23);
+            textBox_selecciona2.TabIndex = 3;
+            textBox_selecciona2.Text = "Selecciona el idioma del elemento";
+            // 
+            // groupBox_RadioButtons
+            // 
+            groupBox_RadioButtons.Controls.Add(radioButtoningles);
+            groupBox_RadioButtons.Controls.Add(radioButtoncatala);
+            groupBox_RadioButtons.Controls.Add(radioButton_castellano);
+            groupBox_RadioButtons.Location = new Point(7, 128);
+            groupBox_RadioButtons.Margin = new Padding(2, 2, 2, 2);
+            groupBox_RadioButtons.Name = "groupBox_RadioButtons";
+            groupBox_RadioButtons.Padding = new Padding(2, 2, 2, 2);
+            groupBox_RadioButtons.Size = new Size(244, 42);
+            groupBox_RadioButtons.TabIndex = 4;
+            groupBox_RadioButtons.TabStop = false;
+            // 
+            // radioButtoningles
+            // 
+            radioButtoningles.AutoSize = true;
+            radioButtoningles.Location = new Point(153, 18);
+            radioButtoningles.Margin = new Padding(2, 2, 2, 2);
+            radioButtoningles.Name = "radioButtoningles";
+            radioButtoningles.Size = new Size(56, 19);
+            radioButtoningles.TabIndex = 2;
+            radioButtoningles.TabStop = true;
+            radioButtoningles.Text = "ingles";
+            radioButtoningles.UseVisualStyleBackColor = true;
+            // 
+            // radioButtoncatala
+            // 
+            radioButtoncatala.AutoSize = true;
+            radioButtoncatala.Location = new Point(90, 19);
+            radioButtoncatala.Margin = new Padding(2, 2, 2, 2);
+            radioButtoncatala.Name = "radioButtoncatala";
+            radioButtoncatala.Size = new Size(56, 19);
+            radioButtoncatala.TabIndex = 1;
+            radioButtoncatala.TabStop = true;
+            radioButtoncatala.Text = "catala";
+            radioButtoncatala.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_castellano
+            // 
+            radioButton_castellano.AutoSize = true;
+            radioButton_castellano.Location = new Point(4, 19);
+            radioButton_castellano.Margin = new Padding(2, 2, 2, 2);
+            radioButton_castellano.Name = "radioButton_castellano";
+            radioButton_castellano.Size = new Size(78, 19);
+            radioButton_castellano.TabIndex = 0;
+            radioButton_castellano.TabStop = true;
+            radioButton_castellano.Text = "castellano";
+            radioButton_castellano.UseVisualStyleBackColor = true;
+            // 
+            // textBox_selecciona3
+            // 
+            textBox_selecciona3.Location = new Point(7, 220);
+            textBox_selecciona3.Margin = new Padding(2, 2, 2, 2);
+            textBox_selecciona3.Name = "textBox_selecciona3";
+            textBox_selecciona3.Size = new Size(246, 23);
+            textBox_selecciona3.TabIndex = 5;
+            textBox_selecciona3.Text = "Selecciona Tablet";
+            // 
+            // comboBox_selecciona_tablet
+            // 
+            comboBox_selecciona_tablet.FormattingEnabled = true;
+            comboBox_selecciona_tablet.Location = new Point(7, 257);
+            comboBox_selecciona_tablet.Margin = new Padding(2, 2, 2, 2);
+            comboBox_selecciona_tablet.Name = "comboBox_selecciona_tablet";
+            comboBox_selecciona_tablet.Size = new Size(246, 23);
+            comboBox_selecciona_tablet.TabIndex = 6;
+            // 
+            // checkBox_element_inicial
+            // 
+            checkBox_element_inicial.AutoSize = true;
+            checkBox_element_inicial.Location = new Point(7, 191);
+            checkBox_element_inicial.Margin = new Padding(2, 2, 2, 2);
+            checkBox_element_inicial.Name = "checkBox_element_inicial";
+            checkBox_element_inicial.Size = new Size(103, 19);
+            checkBox_element_inicial.TabIndex = 7;
+            checkBox_element_inicial.Text = "Element inicial";
+            checkBox_element_inicial.UseVisualStyleBackColor = true;
+            // 
+            // button_atras
+            // 
+            button_atras.Location = new Point(8, 296);
+            button_atras.Margin = new Padding(2, 2, 2, 2);
+            button_atras.Name = "button_atras";
+            button_atras.Size = new Size(87, 26);
+            button_atras.TabIndex = 8;
+            button_atras.Text = "Atrás";
+            button_atras.UseVisualStyleBackColor = true;
+            // 
+            // button_generar
+            // 
+            button_generar.Location = new Point(247, 296);
+            button_generar.Margin = new Padding(2, 2, 2, 2);
+            button_generar.Name = "button_generar";
+            button_generar.Size = new Size(145, 26);
+            button_generar.TabIndex = 9;
+            button_generar.Text = "Confirmar";
+            button_generar.UseVisualStyleBackColor = true;
+            // 
+            // CampsFinal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(396, 329);
+            Controls.Add(button_generar);
+            Controls.Add(button_atras);
+            Controls.Add(checkBox_element_inicial);
+            Controls.Add(comboBox_selecciona_tablet);
+            Controls.Add(textBox_selecciona3);
+            Controls.Add(groupBox_RadioButtons);
+            Controls.Add(textBox_selecciona2);
+            Controls.Add(comboBox_selecciona);
+            Controls.Add(textBox_selecciona);
+            Controls.Add(textBox_from5);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "CampsFinal";
+            Text = "Form5";
+            groupBox_RadioButtons.ResumeLayout(false);
+            groupBox_RadioButtons.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox_from5;
+        private TextBox textBox_selecciona;
+        private ComboBox comboBox_selecciona;
+        private TextBox textBox_selecciona2;
+        private GroupBox groupBox_RadioButtons;
+        private RadioButton radioButtoningles;
+        private RadioButton radioButtoncatala;
+        private RadioButton radioButton_castellano;
+        private TextBox textBox_selecciona3;
+        private ComboBox comboBox_selecciona_tablet;
+        private CheckBox checkBox_element_inicial;
+        private Button button_atras;
+        private Button button_generar;
+    }
+}
