4e5ea81c51b9aa83c989c7d6b984a937a81dbc8e
ASBcep <asaboritb2324@politecnics.barcelona>
2023-10-31 18:59:00 +0100

    disseny form04 "camps"

---
+++ AppEscritori/Camps.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace AppEscritori
+{
+    partial class Camps
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1Autonomia = new System.Windows.Forms.TextBox();
+            this.textBox2CapacitatDiposit = new System.Windows.Forms.TextBox();
+            this.textBox4Cilindrada = new System.Windows.Forms.TextBox();
+            this.textBox3Cicle = new System.Windows.Forms.TextBox();
+            this.textBox5Element = new System.Windows.Forms.TextBox();
+            this.textBox6Envergadura = new System.Windows.Forms.TextBox();
+            this.textBox7Potencia = new System.Windows.Forms.TextBox();
+            this.textBox8KmFets = new System.Windows.Forms.TextBox();
+            this.textBox9SostreVol = new System.Windows.Forms.TextBox();
+            this.textBox10VelocitatNormal = new System.Windows.Forms.TextBox();
+            this.textBox11VelocitatMaxima = new System.Windows.Forms.TextBox();
+            this.textBox12FontEnergia = new System.Windows.Forms.TextBox();
+            this.textBox13FontIngres = new System.Windows.Forms.TextBox();
+            this.textBox14FormaIngres = new System.Windows.Forms.TextBox();
+            this.textBox15LlocFabricacio = new System.Windows.Forms.TextBox();
+            this.textBox16Longitud = new System.Windows.Forms.TextBox();
+            this.textBox17Pes = new System.Windows.Forms.TextBox();
+            this.button1Enrere = new System.Windows.Forms.Button();
+            this.button2Enrere = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1Autonomia
+            // 
+            this.textBox1Autonomia.Location = new System.Drawing.Point(11, 11);
+            this.textBox1Autonomia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1Autonomia.Name = "textBox1Autonomia";
+            this.textBox1Autonomia.Size = new System.Drawing.Size(155, 20);
+            this.textBox1Autonomia.TabIndex = 0;
+            this.textBox1Autonomia.Text = "Autonomia";
+            // 
+            // textBox2CapacitatDiposit
+            // 
+            this.textBox2CapacitatDiposit.Location = new System.Drawing.Point(11, 35);
+            this.textBox2CapacitatDiposit.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2CapacitatDiposit.Name = "textBox2CapacitatDiposit";
+            this.textBox2CapacitatDiposit.Size = new System.Drawing.Size(155, 20);
+            this.textBox2CapacitatDiposit.TabIndex = 1;
+            this.textBox2CapacitatDiposit.Text = "Capacitat dipòsit";
+            // 
+            // textBox4Cilindrada
+            // 
+            this.textBox4Cilindrada.Location = new System.Drawing.Point(11, 83);
+            this.textBox4Cilindrada.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4Cilindrada.Name = "textBox4Cilindrada";
+            this.textBox4Cilindrada.Size = new System.Drawing.Size(155, 20);
+            this.textBox4Cilindrada.TabIndex = 2;
+            this.textBox4Cilindrada.Text = "Cilindrada";
+            // 
+            // textBox3Cicle
+            // 
+            this.textBox3Cicle.Location = new System.Drawing.Point(11, 59);
+            this.textBox3Cicle.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3Cicle.Name = "textBox3Cicle";
+            this.textBox3Cicle.Size = new System.Drawing.Size(155, 20);
+            this.textBox3Cicle.TabIndex = 3;
+            this.textBox3Cicle.Text = "Cicle";
+            // 
+            // textBox5Element
+            // 
+            this.textBox5Element.Location = new System.Drawing.Point(11, 108);
+            this.textBox5Element.Name = "textBox5Element";
+            this.textBox5Element.Size = new System.Drawing.Size(155, 20);
+            this.textBox5Element.TabIndex = 8;
+            this.textBox5Element.Text = "Element";
+            // 
+            // textBox6Envergadura
+            // 
+            this.textBox6Envergadura.Location = new System.Drawing.Point(11, 134);
+            this.textBox6Envergadura.Name = "textBox6Envergadura";
+            this.textBox6Envergadura.Size = new System.Drawing.Size(155, 20);
+            this.textBox6Envergadura.TabIndex = 9;
+            this.textBox6Envergadura.Text = "Envergadura";
+            // 
+            // textBox7Potencia
+            // 
+            this.textBox7Potencia.Location = new System.Drawing.Point(11, 186);
+            this.textBox7Potencia.Name = "textBox7Potencia";
+            this.textBox7Potencia.Size = new System.Drawing.Size(155, 20);
+            this.textBox7Potencia.TabIndex = 10;
+            this.textBox7Potencia.Text = "Potència";
+            // 
+            // textBox8KmFets
+            // 
+            this.textBox8KmFets.Location = new System.Drawing.Point(11, 212);
+            this.textBox8KmFets.Name = "textBox8KmFets";
+            this.textBox8KmFets.Size = new System.Drawing.Size(155, 20);
+            this.textBox8KmFets.TabIndex = 11;
+            this.textBox8KmFets.Text = "Quilòmetres fets";
+            // 
+            // textBox9SostreVol
+            // 
+            this.textBox9SostreVol.Location = new System.Drawing.Point(11, 238);
+            this.textBox9SostreVol.Name = "textBox9SostreVol";
+            this.textBox9SostreVol.Size = new System.Drawing.Size(155, 20);
+            this.textBox9SostreVol.TabIndex = 12;
+            this.textBox9SostreVol.Text = "Sostre màxim de vol";
+            // 
+            // textBox10VelocitatNormal
+            // 
+            this.textBox10VelocitatNormal.Location = new System.Drawing.Point(11, 264);
+            this.textBox10VelocitatNormal.Name = "textBox10VelocitatNormal";
+            this.textBox10VelocitatNormal.Size = new System.Drawing.Size(155, 20);
+            this.textBox10VelocitatNormal.TabIndex = 13;
+            this.textBox10VelocitatNormal.Text = "Velocitat";
+            // 
+            // textBox11VelocitatMaxima
+            // 
+            this.textBox11VelocitatMaxima.Location = new System.Drawing.Point(11, 290);
+            this.textBox11VelocitatMaxima.Name = "textBox11VelocitatMaxima";
+            this.textBox11VelocitatMaxima.Size = new System.Drawing.Size(155, 20);
+            this.textBox11VelocitatMaxima.TabIndex = 14;
+            this.textBox11VelocitatMaxima.Text = "Velocitat màxima";
+            // 
+            // textBox12FontEnergia
+            // 
+            this.textBox12FontEnergia.Location = new System.Drawing.Point(233, 11);
+            this.textBox12FontEnergia.Name = "textBox12FontEnergia";
+            this.textBox12FontEnergia.Size = new System.Drawing.Size(155, 20);
+            this.textBox12FontEnergia.TabIndex = 15;
+            this.textBox12FontEnergia.Text = "Font d\'energia";
+            // 
+            // textBox13FontIngres
+            // 
+            this.textBox13FontIngres.Location = new System.Drawing.Point(233, 35);
+            this.textBox13FontIngres.Name = "textBox13FontIngres";
+            this.textBox13FontIngres.Size = new System.Drawing.Size(155, 20);
+            this.textBox13FontIngres.TabIndex = 16;
+            this.textBox13FontIngres.Text = "Font d\'ingrés";
+            // 
+            // textBox14FormaIngres
+            // 
+            this.textBox14FormaIngres.Location = new System.Drawing.Point(233, 59);
+            this.textBox14FormaIngres.Name = "textBox14FormaIngres";
+            this.textBox14FormaIngres.Size = new System.Drawing.Size(155, 20);
+            this.textBox14FormaIngres.TabIndex = 17;
+            this.textBox14FormaIngres.Text = "Forma d\'ingrés";
+            // 
+            // textBox15LlocFabricacio
+            // 
+            this.textBox15LlocFabricacio.Location = new System.Drawing.Point(233, 83);
+            this.textBox15LlocFabricacio.Name = "textBox15LlocFabricacio";
+            this.textBox15LlocFabricacio.Size = new System.Drawing.Size(155, 20);
+            this.textBox15LlocFabricacio.TabIndex = 18;
+            this.textBox15LlocFabricacio.Text = "Lloc de fabricació";
+            // 
+            // textBox16Longitud
+            // 
+            this.textBox16Longitud.Location = new System.Drawing.Point(233, 108);
+            this.textBox16Longitud.Name = "textBox16Longitud";
+            this.textBox16Longitud.Size = new System.Drawing.Size(155, 20);
+            this.textBox16Longitud.TabIndex = 19;
+            this.textBox16Longitud.Text = "Longitud";
+            // 
+            // textBox17Pes
+            // 
+            this.textBox17Pes.Location = new System.Drawing.Point(233, 134);
+            this.textBox17Pes.Name = "textBox17Pes";
+            this.textBox17Pes.Size = new System.Drawing.Size(155, 20);
+            this.textBox17Pes.TabIndex = 20;
+            this.textBox17Pes.Text = "Pes";
+            // 
+            // button1Enrere
+            // 
+            this.button1Enrere.Location = new System.Drawing.Point(11, 330);
+            this.button1Enrere.Name = "button1Enrere";
+            this.button1Enrere.Size = new System.Drawing.Size(75, 23);
+            this.button1Enrere.TabIndex = 21;
+            this.button1Enrere.Text = "Enrere";
+            this.button1Enrere.UseVisualStyleBackColor = true;
+            // 
+            // button2Enrere
+            // 
+            this.button2Enrere.Location = new System.Drawing.Point(313, 330);
+            this.button2Enrere.Name = "button2Enrere";
+            this.button2Enrere.Size = new System.Drawing.Size(75, 23);
+            this.button2Enrere.TabIndex = 22;
+            this.button2Enrere.Text = "Següent";
+            this.button2Enrere.UseVisualStyleBackColor = true;
+            // 
+            // Formulari_04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 365);
+            this.Controls.Add(this.button2Enrere);
+            this.Controls.Add(this.button1Enrere);
+            this.Controls.Add(this.textBox17Pes);
+            this.Controls.Add(this.textBox16Longitud);
+            this.Controls.Add(this.textBox15LlocFabricacio);
+            this.Controls.Add(this.textBox14FormaIngres);
+            this.Controls.Add(this.textBox13FontIngres);
+            this.Controls.Add(this.textBox12FontEnergia);
+            this.Controls.Add(this.textBox11VelocitatMaxima);
+            this.Controls.Add(this.textBox10VelocitatNormal);
+            this.Controls.Add(this.textBox9SostreVol);
+            this.Controls.Add(this.textBox8KmFets);
+            this.Controls.Add(this.textBox7Potencia);
+            this.Controls.Add(this.textBox6Envergadura);
+            this.Controls.Add(this.textBox5Element);
+            this.Controls.Add(this.textBox3Cicle);
+            this.Controls.Add(this.textBox4Cilindrada);
+            this.Controls.Add(this.textBox2CapacitatDiposit);
+            this.Controls.Add(this.textBox1Autonomia);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Formulari_04";
+            this.Text = "Formulari_04";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1Autonomia;
+        private System.Windows.Forms.TextBox textBox2CapacitatDiposit;
+        private System.Windows.Forms.TextBox textBox4Cilindrada;
+        private System.Windows.Forms.TextBox textBox3Cicle;
+        private System.Windows.Forms.TextBox textBox5Element;
+        private System.Windows.Forms.TextBox textBox6Envergadura;
+        private System.Windows.Forms.TextBox textBox7Potencia;
+        private System.Windows.Forms.TextBox textBox8KmFets;
+        private System.Windows.Forms.TextBox textBox9SostreVol;
+        private System.Windows.Forms.TextBox textBox10VelocitatNormal;
+        private System.Windows.Forms.TextBox textBox11VelocitatMaxima;
+        private System.Windows.Forms.TextBox textBox12FontEnergia;
+        private System.Windows.Forms.TextBox textBox13FontIngres;
+        private System.Windows.Forms.TextBox textBox14FormaIngres;
+        private System.Windows.Forms.TextBox textBox15LlocFabricacio;
+        private System.Windows.Forms.TextBox textBox16Longitud;
+        private System.Windows.Forms.TextBox textBox17Pes;
+        private System.Windows.Forms.Button button1Enrere;
+        private System.Windows.Forms.Button button2Enrere;
+    }
+}
