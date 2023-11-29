0e45a0dc6141c2c3b46b787fc07fffd9e8c4a170
Pau Mateu <148996512+ElStevenn@users.noreply.github.com>
2023-11-29 17:25:33 +0100

    gestion jsons

--- AppEscritori/GestionJSONs.cs
+++ AppEscritori/GestionJSONs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace AppEscritori
 {
-    internal class GestionJSONs
+    public class JsonDataManager
     {
+        private string filePath;
+
+        public JsonDataManager(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<dynamic> LeerJson()
+        {
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<dynamic>>(json);
+            }
+        }
+
+        public void EscribirJson(List<dynamic> data)
+        {
+            using (StreamWriter w = new StreamWriter(filePath))
+            {
+                string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                w.Write(json);
+            }
+        }
+
+        public void AgregarElemento(dynamic elemento)
+        {
+            var data = LeerJson();
+            data.Add(elemento);
+            EscribirJson(data);
+        }
     }
 }
